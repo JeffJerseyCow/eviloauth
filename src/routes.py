7ac175979b7e98d7f9559b1b362aa26bb8bac6d6
Harry Frankl <harryf@cdsec.co.uk>
2023-12-11 18:49:22 +0000

    Made suggested changes

--- src/routes.py
+++ src/routes.py
@@ -4,6 +4,7 @@ from datetime import date, datetime
 import base64
 import json
 from . import flask_app, cache
+from .token_utilities import process_token
 
 @flask_app.route('/')
 def home():
@@ -29,44 +30,23 @@ def add_padding(token_part):
     return token_part + '=' * (4 - len(token_part) % 4)
 
 def get_cache(cache_key):
-     return print(cache.get(cache_key))
+    print(cache.get(cache_key))
 
 @flask_app.route('/callback', methods=['POST'])
 def callback():
-    try:
-        data = request.json
-        token = data.get('access_token')
+    data = request.json
+    token = data.get('access_token')
 
-        if not token:
-            raise ValueError("No token provided")
-
-        payload = decode_access_token(token)
-        unique_name = payload.get('unique_name')
-        if not unique_name:
-            raise ValueError("Unique name not found in token payload")
-
-        scope = payload.get('scp')
-        time = datetime.now().strftime("%H:%M:%S")
-        today = date.today().strftime("%d-%m-%Y")
-
-        cache_key = 'user_data_' + unique_name
-        cache_data = {
-            "access_token": token,
-            "scope": scope,
-            "time": time,
-            "date": today,
-            "email_address": unique_name
-        }
-        cache.set(cache_key, cache_data)
-        get_cache(cache_key)
-        return jsonify({"status": "success", "message": "Token received"})
-
-    except Exception as e:
-        logging.error(f"Error in callback: {e}", exc_info=True)
-        return jsonify({"status": "error", "message": str(e)}), 500
+    if token:
+        try:
+            cache_data = process_token(token)
+            return jsonify({"status": "success", "message": "Token received", "data": cache_data})
+        except ValueError as e:
+            return jsonify({"status": "error", "message": str(e)}), 400
+    else:
+        return jsonify({"status": "error", "message": "No token provided"}), 400
 
 redirect_uri_endpoint = flask_app.config.get('REDIRECT_URI_ENDPOINT')
-
 @flask_app.route(f'/{redirect_uri_endpoint}', methods=['GET'])
 def redirect():
 	final_destination = flask_app.config.get('FINAL_DESTINATION')
